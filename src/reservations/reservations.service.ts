600c2968628dbd0ff993dfe1f3dca27efb875357
pablodexsa <pablodex@gmail.com>
2025-11-18 13:32:36 -0800

    Update Multer type import in reservations service

--- src/reservations/reservations.service.ts
+++ src/reservations/reservations.service.ts
@@ -9,7 +9,7 @@ import { User } from '../users/user.entity';
 import * as fs from 'fs';
 import * as path from 'path';
 import PDFDocument from 'pdfkit';
-import type { File as MulterFile } from 'multer';
+import type * as Multer from 'multer';
 
 
 const pesos = (n: number) =>
@@ -238,7 +238,7 @@ export class ReservationsService {
   async addGuarantor(
     reservationId: number,
     data: { firstName: string; lastName: string; dni: string; address?: string; phone?: string },
-    files: { dniFile?: MulterFile[]; payslipFile?: MulterFile[] },
+    files: { dniFile?: Multer.File[]; payslipFile?: Multer.File[] },
   ) {
     const reservation = await this.findOne(reservationId);
     if (!reservation) throw new NotFoundException('Reserva no encontrada');
